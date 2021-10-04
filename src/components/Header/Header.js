982a332791bda12d2ec090fa8ec42253cab860ee
shoieb-alam <86704871+shoieb-alam@users.noreply.github.com>
2021-10-04 11:17:06 +0600

    banner added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Container, Navbar } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
 
     return (
-        <Navbar className='navbar bg-light'>
+        <Navbar className='navbar bg-light fixed-top'>
             <Container>
-                <Navbar.Brand className='navbar-brand fs-2'>  LEARNING ZONE</Navbar.Brand>
+                <Link to="/" className="text-decoration-none">
+                    <Navbar.Brand className='navbar-brand fs-2'>  LEARNING ZONE</Navbar.Brand>
+                </Link>
+
                 <nav className="navbar-nav ms-auto">
                     <NavLink className='navlink' to="/home">Home </NavLink>
                     <NavLink className='navlink' to="/about">About Us</NavLink>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home</h2>
-        </div>
+        <div className="banner text-white">
+            {/* bannar part */}
+            <div className="bg-color">
+                <div className="text-border">
+                    <h2 className="text-dec fs-3">Learning Zone</h2>
+                </div>
+                <div>
+                    <h4 className="fs-2 my-4">First you Learn, then you remove the 'L'
+                    </h4>
+                    <p className="fs-5 my-4">An easy and organized way to explore the  world of coding through the window of our website
+                    </p>
+
+                    <Button variant="outline-success" size='lg' className="btn border-3 rounded-pill text-white my-4">GIVE US A SUGGESTION</Button>
+                </div>
+            </div>
+        </div >
     );
 };
 
